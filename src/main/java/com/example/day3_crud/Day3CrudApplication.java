f6370003a08353d7fa384e43e6e4f7d85d053a55
khushi1825 <iamkhushi5621@gmail.com>
2026-01-20 11:50:32 +0530

    initial commit

---
+++ src/main/java/com/example/day3_crud/Day3CrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3CrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3CrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day3_crud/Day3CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3CrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
