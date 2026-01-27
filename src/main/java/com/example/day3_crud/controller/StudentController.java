87ed5f626d86c1a53ee8a75714d0d0425cb3943b
khushi1825 <iamkhushi5621@gmail.com>
2026-01-27 15:37:15 +0530

    delete API

--- src/main/java/com/example/day3_crud/controller/StudentController.java
+++ src/main/java/com/example/day3_crud/controller/StudentController.java
@@ -17,12 +17,24 @@ public class StudentController {
     }
 
     @PostMapping("/add-student")
-    public StudentModel addStudent(@RequestBody StudentModel student){
+    public StudentModel addStudent(@RequestBody StudentModel student) {
         return service.addStudent(student);
     }
 
     @GetMapping("/students")
-    public List<StudentModel> getStudents(){
+    public List<StudentModel> getStudents() {
         return service.getStudents();
     }
+
+    @PutMapping("/update/{id}")
+    public StudentModel updateStudent(@PathVariable String id, @RequestBody StudentModel student) {
+        return service.updateStudent(id, student);
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteStudent(@PathVariable String id) {
+        service.deleteStudent(id);
+    }
 }
+
--- src/main/java/com/example/day3_crud/service/StudentService.java
+++ src/main/java/com/example/day3_crud/service/StudentService.java
@@ -3,6 +3,7 @@ package com.example.day3_crud.service;
 import com.example.day3_crud.model.StudentModel;
 import com.example.day3_crud.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
@@ -26,6 +27,27 @@ public class StudentService {
     public List<StudentModel> getStudents(){
         return repository.findAll();
     }
+
+    //update
+    public StudentModel updateStudent(String id,StudentModel student){
+        StudentModel existingStudent =repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No Student found"));
+
+        existingStudent.setName(student.getName());
+        existingStudent.setAge(student.getAge());
+        existingStudent.setEmail(student.getEmail());
+
+        return repository.save(existingStudent);
+    }
+
+    //delete student
+    public void deleteStudent(String id){
+        repository.deleteById(id);
+    }
+
+
+
+
 
 
 }
